50cb8878fd02de94a57d268d61e7946de20721aa
vailstead <jon@glidepathsolutions.com>
2025-05-21 08:01:59 -0400

    Update tasks.py

--- django/drf-celery-uploads/api/tasks.py
+++ django/drf-celery-uploads/api/tasks.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 LOCK_EXPIRE = 60 * 10  # 10 minutes lock expiration, adjust as needed
 
-@shared_task(bind=True)
+@shared_task(bind=True, queue='isolated')
 def process_csv(self, file_path):
     # Placeholder: simulate file processing
     lock_id = "lock:process_csv"
@@ -28,13 +28,14 @@ def process_csv(self, file_path):
             time.sleep(1)
     finally:
         # Release the lock
+        logger.info(f"[INFO] {self.request.id} - Lock release")
         cache.delete(lock_id)
 
     
-@shared_task
-def long_running_task():
+@shared_task(bind=True, queue='default')
+def long_running_task(self):
     # Placeholder: simulate a long-running task
     for i in range(60):
-        logger.info(f"it's a me, mario!: {i+1}%")
+        logger.info(f"{self.request.id} running a long task: {i+1}%")
         # Simulate some processing time
         time.sleep(10)
