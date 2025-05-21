1ffa6d4c1efd3426f75b9f7d3eb0560bebf917b3
vailstead <jon@glidepathsolutions.com>
2025-05-21 07:37:43 -0400

    Create tasks.py

---
+++ django/drf-celery-uploads/api/tasks.py
@@ -0,0 +1,40 @@
+from celery import shared_task
+import time
+import logging
+from django.core.cache import cache
+logger = logging.getLogger(__name__)
+
+LOCK_EXPIRE = 60 * 10  # 10 minutes lock expiration, adjust as needed
+
+@shared_task(bind=True)
+def process_csv(self, file_path):
+    # Placeholder: simulate file processing
+    lock_id = "lock:process_csv"
+    # Try to add the lock key, only if it does NOT exist
+    have_lock = cache.add(lock_id, "true", LOCK_EXPIRE)
+    logger.info(f"[INFO] {self.request.id} - Lock acquired: {have_lock}")
+    logger.info(f"[INFO] {self.request.id} - Lock will be released when task if finished or in {LOCK_EXPIRE} minutes")
+    if not have_lock:
+        # Another task is running
+        logger.info(f"[INFO] Unable to obtain {self.request.id} Lock: {have_lock}. Retrying...")
+        self.retry(countdown=10, max_retries=3)
+        return
+
+    try:
+        # Do your processing here
+        for i in range(60):
+            logger.info(f"Processing {file_path}: {i+1}%")
+            # Simulate some processing time
+            time.sleep(1)
+    finally:
+        # Release the lock
+        cache.delete(lock_id)
+
+    
+@shared_task
+def long_running_task():
+    # Placeholder: simulate a long-running task
+    for i in range(60):
+        logger.info(f"it's a me, mario!: {i+1}%")
+        # Simulate some processing time
+        time.sleep(10)
