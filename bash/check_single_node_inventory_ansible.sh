bfd5fd87eee4089d3a402b8ceda1d939e2e80bfb
vailstead <jon@glidepathsolutions.com>
2025-07-30 11:05:22 -0400

    Update check_single_node_inventory_ansible.sh

--- bash/check_single_node_inventory_ansible.sh
+++ bash/check_single_node_inventory_ansible.sh
@@ -3,3 +3,6 @@ if [ "$(ansible all --inventory your_inventory_file --list-hosts | tail -n +2 | 
 else
   echo "Playbook skipped: inventory has more than one host."
 fi
+
+
+# kubectl get clusters.management.cattle.io -o json | jq -r '.items[] | select(.metadata.annotations["management.cattle.io/cluster-display-name"] == "rke2-prod") | .metadata.name'
